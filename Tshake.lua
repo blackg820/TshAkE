c79b48166b0c2668b80ea11cc085deb7082afa9f
blackg820 <blackg820@gmail.com>
2020-08-13 18:34:09 +0300

    Update Tshake.lua

--- Tshake.lua
+++ Tshake.lua
@@ -4218,7 +4218,7 @@ x = x + 1
 if database:get(bot_id.."Tshake:User:Name"..v.user_id_) then
 t = t..""..x.." - {[@"..database:get(bot_id.."Tshake:User:Name"..v.user_id_).."]}\n"
 else
-t = t..""..x.." - {"..v.user_id_.."}\n"
+t = t..""..x.." - {["..v.user_id_.."](tg://user?id="..v.user_id_..")}\n"
 end
 end
 send(msg.chat_id_,msg.id_,t)
